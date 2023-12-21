2cdc74115f58ef8231b9a73b39f330c9824418bf
Mike <mikesmallhelp@gmail.com>
2023-12-21 18:18:56 +0200

    HTTP 500 error app

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FrontPage from '../components/FrontPage';
+
+const IndexComponent = () => {
+  return (
+    <>
+      <FrontPage pageDirectory='api-returns-http-500' />
+    </>
+  );
+};
+
+export default IndexComponent;
